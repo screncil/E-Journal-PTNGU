b5c547fe6fef13785a74fd655da4df3da305117e
screncil <alexplay1998s3@gmail.com>
2024-08-19 18:44:55 +0300

    realized app

--- backend/backend/grades/models.py
+++ backend/backend/grades/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from subject.models import Subjects
+from users.models import User
+
 # Create your models here.
+
+
+
+class Grades(models.Model):
+    id = models.AutoField(primary_key=True)
+    grade = models.IntegerField(null=False, blank=False)
+    subject = models.ForeignKey(Subjects, on_delete=models.CASCADE)
+    theme = models.TextField()
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'status': 'student'},
+    )
