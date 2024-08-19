76eb005b424062ca61b585947a1fa93e22763d72
screncil <alexplay1998s3@gmail.com>
2024-08-19 18:46:37 +0300

    realized subject app

--- backend/backend/subject/admin.py
+++ backend/backend/subject/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Subjects
+
 # Register your models here.
+
+admin.site.register(Subjects)
--- backend/backend/subject/models.py
+++ backend/backend/subject/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subjects(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    course = models.IntegerField()
+
+    def __str__(self):
+        return self.name
