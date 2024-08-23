aede8f9c0af65d298a37ad23e8ff76f63dc67889
screncil <alexplay1998s3@gmail.com>
2024-08-23 18:34:56 +0300

    added specializtion field to group

--- backend/backend/subject/models.py
+++ backend/backend/subject/models.py
@@ -7,7 +7,7 @@ class Subjects(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     course = models.IntegerField()
-    specializtion = models.IntegerField()
+    specializtion = models.IntegerField(null=False, blank=False)
 
     def __str__(self):
         return self.name
