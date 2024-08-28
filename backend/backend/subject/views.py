da9bc98d44ca62427346e8e967ee9b19532204c7
screncil <alexplay1998s3@gmail.com>
2024-08-28 15:34:59 +0300

    created list subject view

--- backend/backend/subject/views.py
+++ backend/backend/subject/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from .models import Subjects
+from subject.serializers import SubjectSerializer
+from users.permissions import IsTeacher
+
+
+class ListSubjectView(ListAPIView):
+    serializer_class = SubjectSerializer
+    queryset = Subjects.objects.all()
+    permission_classes = [IsTeacher, IsAdminUser]
+    authentication_classes = [TokenAuthentication]
