a041bdd872c0ab011ca99413e98a4ef32aaa7c0d
screncil <alexplay1998s3@gmail.com>
2024-08-28 15:24:42 +0300

    added settings to REST_FRAMEWORK

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -34,7 +34,18 @@ API_VERSION = 'v1'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
 }
 
 # Application definition
